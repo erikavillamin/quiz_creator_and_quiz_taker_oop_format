e23cb22361e839a826585a31de5e5b1057a8e45b
erikavillamin <erikamaevillamin@gmail.com>
2025-05-22 23:27:41 +0800

    prompts_user_to_enter_question

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -4,3 +4,8 @@ class QuizCreator:
     def __init__(self, filename="quiz_questions.txt"):
         self.filename = filename
     
+    def quiz_question(self):
+        question = input(emoji.emojize("Enter a question :thinking_face:: "))
+        if not question.lower().startswith("question:"):
+            question = "Question: " + question
+        return question
