5742638c3a54a586bf7e48dca25adbe5689d5ea6
erikavillamin <erikamaevillamin@gmail.com>
2025-05-22 23:49:24 +0800

    save_question_answer_choices_to_the_file

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -24,3 +24,17 @@ class QuizCreator:
                 return correct_ans
             else:
                 print(emoji.emojize("Invalid. Please enter a, b, c, or d only. :police_car_light:"))
+
+    def save_question(self, question, choices, correct_ans):
+        file_save = input("Do you want to save this question? (yes/no): ").strip().lower()
+        if file_save in ["yes", "y"]:
+            with open(self.filename, "a") as file:
+                file.write(question + "\n")
+                file.write("a) " + choices[0] + "\n")
+                file.write("b) " + choices[1] + "\n")
+                file.write("c) " + choices[2] + "\n")
+                file.write("d) " + choices[3] + "\n")
+                file.write("Answer: " + correct_ans + "\n\n")
+            print(emoji.emojize("Question saved! :grinning_face_with_smiling_eyes:\n"))
+        else:
+            print(emoji.emojize("Question not saved. :frowning_face:\n"))
